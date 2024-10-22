7ccdedd461317e286a702626197803962873bbd0
Sukumar Reddy B <sukumar1118@gmail.com>
2024-10-22 23:45:50 +0530

    React Inception

--- App.js
+++ App.js
@@ -1,4 +1,20 @@
-/*
+/** @@ Sample Hello world program with React @@
+ * 
+ * const header = React.createElement(
+  "h1",
+  { id: "header", datah1: "d1" },
+  "Hello world form React App!"
+    );
+    const rootSample = ReactDOM.createRoot(document.getElementById("root"));
+    console.log("header: ", parent);
+    console.log("root: ", root);
+    root.render(header);
+ * 
+ */
+
+
+/** @@ Nested elements sample @@
+ * 
     <div id="parent">
         <div id="child">
             <h1>I am h1 tag</h1>
@@ -9,16 +25,10 @@
             <h2>I am h1 tag</h2>
         </div>
     </div>
-
-
+ *
 */
 
-// const header = React.createElement(
-//   "h1",
-//   { id: "header", datah1: "d1" },
-//   "Hello world form React App!"
-// );
-
+// Create netsed elements, muliple child elements in an array sample.
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I am h1 tag"),
